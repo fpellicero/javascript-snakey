cc962251897fe38833d8d88a3fa072c5d32daf22
Francesc Pellicero <pelly.obn91@gmail.com>
2016-12-09 19:43:35 +0100

    Added a Node.js Express server to serve the game

---
+++ main.js
@@ -0,0 +1,15 @@
+const path = require('path');
+var express = require('express');
+var app = express();
+
+app.use("/js", express.static(path.join(__dirname, "js")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/css", express.static(path.join(__dirname, "css")));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, function() {
+  console.log("Server Started");
+})
