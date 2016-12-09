42432f18896dcfe99d435a9f6dc8e6a2b0a120fe
Francesc Pellicero <pelly.obn91@gmail.com>
2016-12-09 19:53:45 +0100

    Change port

--- main.js
+++ main.js
@@ -10,6 +10,6 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT, function() {
   console.log("Server Started");
 })
